0c39a3f42b5ccc2f7fa19e4dd55a40fe5f4324e9
Thomas Glass <me@tom.glass>
2021-03-25 19:05:35 +0000

    single item id .05

--- pipeline.py
+++ pipeline.py
@@ -54,11 +54,11 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210325.04'
+VERSION = '20210325.05'
 USER_AGENT = 'Archive Team (ircs://irc.hackint.org#nintendone https://webirc.hackint.org/#irc://irc.hackint.org/#nintendone)'
 TRACKER_ID = 'super-mario-maker-bookmarks'
 TRACKER_HOST = 'legacy-api.arpa.li'
-MULTI_ITEM_SIZE = 30
+MULTI_ITEM_SIZE = 1
 
 
 ###########################################################################
