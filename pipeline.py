5df3e50c7cfe6cd53767148c432400399c50ecf5
OrIdow6 <68304414+OrIdow6@users.noreply.github.com>
2021-03-25 17:12:37 -0700

    Bump version

--- pipeline.py
+++ pipeline.py
@@ -54,7 +54,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210325.05'
+VERSION = '20210326.01'
 USER_AGENT = 'Archive Team (ircs://irc.hackint.org#nintendone https://webirc.hackint.org/#irc://irc.hackint.org/#nintendone)'
 TRACKER_ID = 'super-mario-maker-bookmarks'
 TRACKER_HOST = 'legacy-api.arpa.li'
